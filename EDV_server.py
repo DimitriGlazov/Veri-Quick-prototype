9b65a12dbc8e2079ca2daf97b0d317e50e6e6cd5
Dave <136987161+DimitriGlazov@users.noreply.github.com>
2024-11-06 14:53:22 +0530

    Update EDV_server.py

--- EDV_server.py
+++ EDV_server.py
@@ -101,8 +101,8 @@ def generate_qr_code_with_metadata(files_metadata):
     return img
 
 # Main Streamlit App
-st.title("Veriquick - Aadhaar and PAN Document Scanner")
-st.write("Upload documents to detect Aadhaar and PAN numbers and generate QR codes.")
+st.title("Veriquick ✅")
+st.write("Let's make it paperless.")
 
 uploaded_files = st.file_uploader("Upload PDF documents", type="pdf", accept_multiple_files=True)
 
